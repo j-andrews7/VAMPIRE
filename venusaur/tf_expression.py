a03ba08749b3a4cb4e4c71e1e1f5b6f57391f2c8
Jared Andrews <jared.andrews07@gmail.com>
2016-08-28 15:44:09 -0500

    More work to support multi-sample vcf
    All in tf_expression.py. Getting closer to completion.

--- venusaur/tf_expression.py
+++ venusaur/tf_expression.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 For a given motif annotated vcf file (already run through motifs.py)
-remove all motif matches for TFs that are not expressed in at least one sample.
+remove all motif matches for TFs that are not expressed in at least one sample above the threshold.
     
 Usage: tf_expression.py -i <input.vcf> -e <expression.bed> -o <output.txt> [OPTIONS]
 
@@ -10,107 +10,113 @@ Args:
     -o (required) <output.vcf>: Name of output file to be created.
     -e (required) <expression.bed>: An expression 'bed' file.
     -th (optional) <5>: TFs are considered expressed if they are above this threshold. 
-    -fe (optional flag): If used, variants that do not match any motif for 
-        an expressed protein will not be included in the output file.
+    -fe (optional) <True>: If True, variants that do not match any motif for an expressed protein 
+        will not be included in the output file. Set to False to print original line for variants 
+        that lie in motifs for TFs that are expressed above the threshold.
 """
 import sys
 import argparse
-from math import ceil
-from math import log10
 
 
 def parse_header(line):
-# TODO - Get sample names and list of columns for each
-
-def parse_line(line):
     """
-    Parse a vcf record and return various info.
+    Get all sample names present in the vcf header. 
+
+    Assume each column is in format 'extra_info.samplename'.
+
+    Args:
+        line (str): Header line.
+
+    Returns:
+        samples (list): List of sample names.
+    """
+    line = line.strip().split("\t")[9:]
+    samples = []
+
+    for x in line:
+        samp = x.split('.')[-1]
+        if samp not in samples:
+            samples.append(samp)
+
+    return samples
+
+
+# TODO - Function to get lists of motifs above threshold and below for a list of motif names & 
+# the gene dict.
+
+def process_line(line, filt, output_f, gene_dict):
+    """
+    Process a vcf record and print to output.
 
     Args: 
-        opened_file (file object): An open .vcf file. 
-
-    Returns: 
-        motifsn (list): List of motif names.
-        motif_other (list): List of lists containing other motif info fields.
-            each other field has the INFO name as the first object
-            ex: ["MOTIFV",["0.5", "0.24"]] -> MOTIFV=0.5;0.24
-        other_info (list): List of other INFO fields.
-        line (str): Original line.
+        line (str): Line to parse.
+        filt (bool): True if records that don't have a motif for a TF that's expressed
+            should be excluded from output. False if they should still be included.
+        output_f (file object): Output file. 
+        gene_dict (dict): Dict with gene names as keys and expression values for samples in vcf as
+            values.
     """
     line = line.strip()
-
-    # Find list of motifs that match
     line_list = line.split("\t")
     info_fields = line_list[7].split(";")
-    # Holds other fields (other than motif fields which may be modified)
-    other_info = []
-    (motifns, motif_other) = ([], [])
+    (motifns, motif_other, other_info) = ([], {}, [])
 
     for field in info_fields:
         if field.startswith("MOTIFN="):
-            # Cut off 'MOTIFN=' then convert to an array
+            # Cut off 'MOTIFN=' then create a list.
             motifns = field[7:].split(',')
-        # Check for other motif fields such as:
-        #(MOTIFN)   MOTIFV  MOTIFR  MOTIFC
-        # MOTIFVH   MOTIFRH MOTIFVG MOTIFRG
+        # Create dict from other motif fields.
         elif (field.startswith("MOTIFV=") or field.startswith("MOTIFR=") or
               field.startswith("MOTIFC=")):
             name = field[:7]
             values = field[7:].split(',')
-            motif_other.append([name, values])
+            motif_other[name] = values
         elif (field.startswith("MOTIFVH=") or field.startswith("MOTIFRH=") or
               field.startswith("MOTIFVG=") or field.startswith("MOTIFRG=")):
             name = field[:8]
             values = field[8:].split(',')
-            motif_other.append([name, values])
+            motif_other[name] = values
         else:
             other_info.append(field)
 
-    return (motifns, motif_other, other_info, line)
+    return 
 
 
-def get_genes(opened_file, sample, threshold):
+def get_genes(exp_file, samples, threshold):
     """
     Reads in and parses the .bed expression file.
 
     Args: 
-        opened_file = an already open input .bed file
-        sample (str): Name of the sample in question.
+        exp_file (str): Name of expression file.
+        samples (list): Names of the samples in the vcf file.
         threshold (float): Expression threshold to filter lowly/unexpressed genes.
 
     Returns: 
-        gene_dict = (str -> float) a dictionary of:
-        gene names -> expression levels
+        gene_dict (dict): {gene_name: [expression_vals]. Only include values for samples in the vcf.
     """
+    data_cols = []  # Used to hold indices for columns for which to get expression data.
+    gene_dict = {}
 
-    # Get sample column.
-    # TODO - Only get expression values for samples in VCF.
+    with open exp_file as f:
+        header = f.readline().strip().split('\t')
+        for samp in header[4:]:
+            if samp in samples:
+                data_idx = header.index(samp)
+                data_cols.append(data_idx)
 
-    headers = opened_file.readline().strip().split('\t')
-    sample_idx = -1
-    for idx in range(len(headers)):
-        if headers[idx].upper() == sample.upper():
-            sample_idx = idx
-            break
+        # Read in expression levels for each gene.
+        for line in f:
+            line = line.strip().split('\t')
+            gene_name = line[3].upper()
+            exp_vals = []
+            for idx in data_cols:
+                exp_vals.append(line[idx])
 
-    if sample_idx == -1:
-        print("Err: Sample <" + sample + "> not found in input file header.")
-        return []
+            passed_filter = [x for x >= threshold in exp_vals]
+            if len(passed_filter) > 0:
+                gene_dict[gene_name] = exp_vals
 
-    gene_names = []
-    exp_values = []
-
-    # Read in expression levels for each gene
-    for line in opened_file:
-        gene_name = line.strip().split('\t')[3].upper()
-        # Only add the gene to the list if its expression is above the threshold.
-        exp_level = float(line_list[sample_idx])
-        if exp_level >= threshold:
-            # Add gene names and expression values to respective lists
-            gene_names.append(gene_name)
-            exp_values.append(exp_level)
-
-    return dict(zip(gene_names, exp_values))
+        return gene_dict
 
 
 def print_record(line_tup, output_f, filt):
@@ -167,7 +173,7 @@ def print_record(line_tup, output_f, filt):
             outline += "\t"
         if idx == 7:
             if len(motifns) != 0:
-                outline += "MOTIFN=" + names + ";MOTIFE=" + explevels
+                outline += "MOTIFN=" + names
                 for idy in range(len(motifos)):
                     outline += ";" + motifos[idy][0] + str_m_os[idy]
                 for field in oth_info:
@@ -189,35 +195,33 @@ def print_record(line_tup, output_f, filt):
 
 
 def main(inp_file, exp_file, out_file, th, filt):
-    # TODO - Get sample names.
 
-    gene_dict = []
-    with open(exp_file) as opened_ef:
-        gene_dict = get_genes(opened_ef, sample_name, th)
 
-    if len(gene_dict) == 0:
-        print("Error, no genes above threshold found in expression file.")
-        sys.exit()
-
-    # Open output file.
     output_f = open(out_file, "w")
 
     with open(inp_file) as vcf:
 
-        line = vcf.readline()
+        line = vcf.readline().strip()
 
         # Skip info lines.
         while line.startswith("##"):
-            print(line, file=output_f, end="")
-            line = vcf.readline()
+            print(line, file=output_f)
+            line = vcf.readline().strip()
 
-        # First non-## line is the header.
-        # TODO - Parse header.
-        print(line, file=output_f, end="")
+        # First non-## line is the header. Get sample names and print to output.
+        samples = parse_header(line)
+        print(line, file=output_f)
+
+        gene_dict = get_genes(exp_file, sample_names, th)
+
+        if len(gene_dict) == 0:
+            print("Error, no genes above threshold found in expression file.",
+                "\nTry lowering the threshold and ensure the expression file",
+                " has values in the range that you expect.")
+            sys.exit()
 
         for line in vcf:
-            current_var = parse_line(line)
-
+            current_var = process_line(line, filt, output_f)
 
             (motifns, motifos, oi, line) = current_var
 
@@ -231,9 +235,6 @@ def main(inp_file, exp_file, out_file, th, filt):
                 # ["MOTIFC", ['Y','N','Y','Y']] -> MOTIFC=Y,N,Y,Y
                 info_name = motifos[idx][0]
                 f_motifos.append([info_name, []])
-
-            # Expression level of motifs
-            f_motifes = []
 
             for idx in range(len(motifns)):
                 motif_name = motifns[idx].upper()
@@ -250,8 +251,8 @@ def main(inp_file, exp_file, out_file, th, filt):
                         f_motifos[idy][1].append(motifos[idy][1][idx])
 
             # Output filtered motifs
-            tup = (f_motifns, f_motifos, f_motifes, oi, line)
-            print_record(tup, output_f, options)
+            tup = (f_motifns, f_motifos, oi, line)
+            print_record(tup, output_f, filt)
 
             current_var = get_next_var(vcf)
 
