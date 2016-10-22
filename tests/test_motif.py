1fa5ecb4379a3ac6c857b411c340fd0bd2aea95c
Crumbs350 <wkrekeler@yahoo.com>
2016-10-21 21:02:20 -0500

    fixed to match change in motif.py variable names

--- tests/test_motif.py
+++ tests/test_motif.py
@@ -9,27 +9,27 @@ y = motif.motifArray()
 for i in range(4):
     x = motif.motifElement()
     x.positions = i
-    y.addMotif(x)
+    y.add_motif(x)
 
 y.motifs[3].positions
 y.max_positions
-y.max_positionsIndex
+y.max_positions_index
 
 x.positions = 13
-y.addMotif(x)
+y.add_motif(x)
 y.max_positions
-y.max_positionsIndex
+y.max_positions_index
 
 x.positions = 2
-y.addMotif(x)
+y.add_motif(x)
 y.max_positions
-y.max_positionsIndex
+y.max_positions_index
 
 y.length()
 
-y.deleteMotif(4)
+y.delete_motif(4)
 y.max_positions
-y.max_positionsIndex
+y.max_positions_index
 y.length()
 
 x = motif.motifElement()
@@ -41,11 +41,26 @@ x.clear()
 print((x.name + ":" + format(x.positions) + " post clear"))
 
 #x.matrix = [[] for x in range(4)]
-x.validFlag
-x.checkValid()
-x.validFlag
+x.valid_flag
+x.check_valid()
+x.valid_flag
 x.matrix[1] = [3, 4, 5]
 x.matrix[2] = [3, 4, 5, 6]
-x.checkValid()
-x.validFlag
+x.check_valid()
+x.valid_flag
 
+
+# -- must create files in path using touch, and text editor for test to work
+import motif
+x = motif.get_baseline_probs( 'emptyfile' )
+#**ERROR** Empty file found.
+#File should contain only [ PrA PrC PrG PrT ]
+#Where PrA + PrC + PrG + PrT = 1 (and all are positive and non-zero)
+#Continuing with default probabilities:[0.25, 0.25, 0.25, 0.25]
+
+x = motif.get_baseline_probs( 'notemptyfile' )
+#**ERROR** Baseline probability file incorrectly formatted.
+#File should contain only [ PrA PrC PrG PrT ]
+#Where PrA + PrC + PrG + PrT = 1 (and all are positive and non-zero)
+#Continuing with default probabilities:[0.25, 0.25, 0.25, 0.25]
+x = motif.get_baseline_probs( 'notemptyfile_correct')
